f75b5c6653680c5a4aad9ea2c95830beb14d203a
Valerio Salzano <vlr.slzn@gmail.com>
2023-03-17 18:27:38 +0100

    right dates printed on messages and contacts list

--- js/main.js
+++ js/main.js
@@ -3,13 +3,27 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        contacts: contacts,
+        contacts: {...contacts},
         selected: 0,
       }
     },
     methods: {
       clickOnContact(index){
         this.selected = index;
-      }
+      },
+      getMsgTime(message){
+        // data in dd/mm/yyyy bisogna riordinare per ottenere mm/dd/yyyy
+        let msgDate = message.date;
+        msgDate = msgDate.split('/');
+        msgDate = msgDate[1]+'/'+msgDate[0]+'/'+msgDate[2];
+        // data riordinata
+        msgDate = new Date (msgDate);
+        msgDate = msgDate.getHours()+':'+msgDate.getMinutes();
+        return msgDate;
+      },
+      getLastMsgTime(index){
+        let msg = contacts[index].messages[this.contacts[index].messages.length-1];
+        return this.getMsgTime(msg);
+      },
     }
   }).mount('#app');
