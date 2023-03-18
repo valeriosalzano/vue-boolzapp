0203caf4e2650da135369bd2e265c0e22eac2b9e
Valerio Salzano <vlr.slzn@gmail.com>
2023-03-18 17:47:55 +0100

    milestone 5 concluso

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ const { createApp } = Vue
         msgBarPlaceholder : 'Scrivi un messaggio',
         searchBarInput : '' ,
         selectedMsg: -1,
+        printDateLineVar : '000',
+        printDateVar: '',
       }
     },
     methods: {
@@ -22,20 +24,23 @@ const { createApp } = Vue
       },
       // funzione che restituisce solo ora e minuti dalla stringa della data
       getMsgTime(message){
-        let msgTime = message.date;
-        return msgTime.substring(msgTime.length - 8, msgTime.length - 3);
+        return luxon.DateTime.fromFormat(message.date,'dd/MM/yyyy HH:mm:ss').toFormat('HH:mm');
       },
+      // funzione che gestisce l'invio di un messaggio
       sendMsg(){
         this.contacts[this.selected].messages.push(
           { 
-          date: '10/01/2020 15:30:55',
+          date: luxon.DateTime.now().toFormat('d/MM/yyyy HH:mm:ss'),
           message: this.userMsg,
           status: 'sent'
           }
         );
         this.userMsg = '';
         this.autoReply();
+        this.goToLastMsg(200);
+
       },
+      // funzione che fa rispondere dopo 1 secondo il contatto con "ok" quando si invia un messaggio
       autoReply(){
         // salvo il selected in una variabile per evitare che il timeout stampi su un contatto diverso
         let selected = this.selected;
@@ -43,14 +48,16 @@ const { createApp } = Vue
         setTimeout(()=>{
           this.contacts[selected].messages.push(
             { 
-            date: '10/01/2020 15:30:55',
+            date: luxon.DateTime.now().toFormat('dd/MM/yyyy HH:mm:ss') ,
             message: 'ok',
             status: 'received'
             }
           );
           this.msgBarPlaceholder = 'Scrivi un messaggio';
         },1000)
+        this.goToLastMsg(1200);
       },
+      // funzione che gestisce il funzionamento della search bar 
       onContactSearching(){
         if(!this.searchBarInput){
           // caso 1: campo vuoto, scorro tutti i contatti e li rendo visibili
@@ -60,15 +67,42 @@ const { createApp } = Vue
           this.contacts.forEach( contact => contact.visible = contact.name.match(new RegExp(`${this.searchBarInput}`,'i')))
         }
       },
+      // funzione che recupera l'indice del messaggio su cui si clicca rendendo visibile il menu
       clickOnMsg(index){
         this.selectedMsg = index;
       },
+      // funzione che elimina il messaggio al click
       clickOnDeleteMsg(index){
         this.contacts[this.selected].messages.splice(index,1);
         this.resetSelectedMsg();
       },
+      // funzione che fa scomparire il menu del messaggio
       resetSelectedMsg(){
         this.selectedMsg = -1;
+      },
+      // funzione che evidenza l'ultimo messaggio scrollando fino a raggiungerlo
+      goToLastMsg(timer){
+        const lastMsgArray = document.querySelectorAll('.msg');
+        const lastMsg =lastMsgArray[lastMsgArray.length-1];
+        setTimeout( ()=>{
+          lastMsg.scrollIntoView({behavior:"smooth"});
+        },timer);
+      },
+      printDateLine(msg,index){
+        if (index == 0){
+          this.printDateLineVar = '000';
+        }
+        let msgDate = luxon.DateTime.fromFormat(msg.date,'dd/MM/yyyy HH:mm:ss').toFormat('dd-MM-yyyy');
+        if( this.printDateLineVar != msgDate){
+          this.printDateLineVar = msgDate
+          return true
+        } else {
+          return false;
+        }
+      },
+      printDate(msg){
+        let date = luxon.DateTime.fromFormat(msg.date,'dd/MM/yyyy HH:mm:ss').toFormat('dd MMM yyyy');
+        return date;
       }
     }
   }).mount('#app');
